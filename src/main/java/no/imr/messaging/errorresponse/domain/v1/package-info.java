bc302ed76da26f42bb9a0e8d17bee9f6966c42f9
Sjur Ringheim Lid <sjur.ringheim.lid@imr.no>
2015-09-17 13:33:53 +0200

    First version of error response xsd

---
+++ src/main/java/no/imr/messaging/errorresponse/domain/v1/package-info.java
@@ -0,0 +1,7 @@
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.imr.no/formats/errorresponse/v1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = {
+    @javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.imr.no/formats/errorresponse/v1", prefix = "")
+})
+
+package no.imr.messaging.errorresponse.domain.v1;
+
